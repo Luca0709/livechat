2d37cf6a12a7061b3d20629362ea21779950a2ca
Zraune97 <heimo.zraunig777@gmail.com>
2025-05-15 21:09:55 +0200

    halloffame init

--- client/script.js
+++ client/script.js
@@ -1,41 +1,48 @@
 const socket = io();
 let nickname = "";
 
-// Funktion zum Login
 function login() {
   nickname = document.getElementById('nickname').value.trim();
   if (!nickname) return alert("Bitte Namen eingeben");
 
   document.getElementById('login').style.display = 'none';
-  document.getElementById('chat').style.display = 'block';
+  document.getElementById('chat').style.display = 'flex';
+  document.body.classList.add('chat-open');
 }
 
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 const messages = document.getElementById('messages');
 
-// Wenn der Benutzer eine Nachricht sendet
 form.addEventListener('submit', e => {
   e.preventDefault();
   if (input.value && nickname) {
-    // Sende Nachricht an den Server
     socket.emit('chat message', { name: nickname, text: input.value });
     input.value = '';
   }
 });
 
-// Wenn eine Nachricht vom Server kommt (auch alte Nachrichten)
 socket.on('chat message', msg => {
-  // Prüfen, ob der Text der Nachricht nicht leer ist
   if (msg && msg.text && msg.name) {
     const item = document.createElement('li');
-    item.textContent = `[${msg.name}] ${msg.text}`;
 
-      if (msg.name === nickname) {
-        item.classList.add('own-message');
-      }
+    const msgText = document.createElement('span');
+    msgText.innerHTML = `<strong>[${msg.name}]</strong> ${msg.text}`;
+
+    const btnHallOfFame = document.createElement('button');
+    btnHallOfFame.classList.add('btn', 'btn-info');
+    btnHallOfFame.textContent = 'HallOfFame';
+    btnHallOfFame.onclick = () => saveMsgIntoHallOfFame(msg);
+
+    item.appendChild(msgText);
+    item.appendChild(btnHallOfFame);
 
     messages.appendChild(item);
-    window.scrollTo(0, document.body.scrollHeight); // Automatisch nach unten scrollen
+    messages.scrollTop = messages.scrollHeight;
   }
 });
+
+
+function saveMsgIntoHallOfFame(msg) {
+  alert("TO BE IMPLEMENTED. MSG: " + msg.text);
+}
